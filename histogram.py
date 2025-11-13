7eba7717bfc71cdd417aafdfae97b543ce553892
bartulo <bartulo@gmail.com>
2025-11-13 11:11:41 +0000

    first commit

---
+++ histogram.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def make_plot(ins, outs):
+    print('hola')
+    x = ins['HeightAboveGround']
+
+    plt.hist(x, bins=30, color='green', edgecolor='black')
+
+    plt.savefig(f"histogram.png")
+    
+    return True
+
+def make_model_histogram(ins, outs):
+    print('hola')
+    x = ins['HeightAboveGround']
+    plt.hist(x, bins=30, weights=1 / pdalargs['m2'] * np.ones(len(x)), color='green', edgecolor='black')
+    plt.ylim(0, 30)
+    plt.savefig(f"histogram_{pdalargs['vuelo']}_zona{pdalargs['zona']}_modelo{pdalargs['modelo']}.png")
+    plt.close()
+    return True
+
+def make_evol_histogram(ins, outs):
+    x = ins['HeightAboveGround']
+    plt.hist(x, bins=30, weights=1 / pdalargs['m2'] * np.ones(len(x)), color='green', edgecolor='black')
+    plt.ylim(0, 30)
+    plt.savefig(f"evolucion_histogram_{pdalargs['vuelo_pre']}_{pdalargs['vuelo_post']}_zona{pdalargs['zona']}_modelo{pdalargs['modelo']}.png")
+    plt.close()
+    return True
