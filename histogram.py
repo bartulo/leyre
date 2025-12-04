4e60dfd42a9a2d86c49387f62e5d03687c56162f
bartulo <bartulo@gmail.com>
2025-12-04 11:20:50 +0000

    biomasa

--- histogram.py
+++ histogram.py
@@ -2,17 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def make_plot(ins, outs):
-    print('hola')
     x = ins['HeightAboveGround']
 
     plt.hist(x, bins=30, color='green', edgecolor='black')
 
-    plt.savefig(f"histogram.png")
+    plt.savefig(f"FINALES/HISTOGRAM/histogram_{pdalargs['vuelo']}_zona{pdalargs['zona']}.png")
     
     return True
 
 def make_model_histogram(ins, outs):
-    print('hola')
     x = ins['HeightAboveGround']
     plt.hist(x, bins=30, weights=1 / pdalargs['m2'] * np.ones(len(x)), color='green', edgecolor='black')
     plt.ylim(0, 30)
